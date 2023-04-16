ef70ba156fb8334a8777a5eaa3e813758b9f266f
Ronny Perez <ronnypdev@gmail.com>
2023-04-16 17:36:49 -0400

    Added Store starter files

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import adastra from 'adastra-plugin'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [adastra(), react()],
+  build: {
+    emptyOutDir: false
+  }
+})
